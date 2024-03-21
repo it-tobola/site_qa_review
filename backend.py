77b2c0d453f3b1117f317aa9b131d3918341b4eb
it-tobola <it@tobolainc.com>
2024-03-21 08:34:49 -0400

    streamlined the application to one python file.

---
+++ backend.py
@@ -0,0 +1,2 @@
+password = "Tobola#22"
+file_location = fr"C:/Users/olato/OneDrive/Automated QA Site Reviews/"
--- pages/final_review.py
+++
@@ -1,2 +0,0 @@
-import streamlit as st
-from st_pages import Page, show_pages, hide_pages
